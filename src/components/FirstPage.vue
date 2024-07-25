8d4b11886ed0e560f552ade4f6ade69a7bf50855
markobakic99 <marko.bakic@kentkart.rs>
2024-07-25 15:33:39 +0200

    first commit

---
+++ src/components/FirstPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <h1>firstpage</h1>
+  </q-page>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { Quasar } from 'quasar';
+import 'quasar/dist/quasar.css'; // Uvezite Quasar stilove
+import { QBtn } from 'quasar';
+import { QTable } from 'quasar';
+import { QDialog } from 'quasar';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Quasar);
+app.component('q-btn',QBtn)
+app.component('q-table',QTable)
+app.component('q-dialog',QDialog)
+
+app.mount('#app');
